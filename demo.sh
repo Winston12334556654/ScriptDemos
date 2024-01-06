3c6b991061db8b28f017ec3d6d4a3a48f33c0c29
Winston12334556654 <marannoe1322001@gmailcom>
2024-01-07 01:39:08 +0630

    Adding toilet and cmatrix for both linux and mac os.

--- demo.sh
+++ demo.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+sudo apt-get install toilet
 brew install toilet 
+sudo apt-get install cmatrix
+brew install cmatrix
+
 if [ $? -ne 0 ];
 then 
 	echo "Heee Heee"
 exit 1
 fi 
 toilet "Winston Hacked Your System!"
+toilet "No System is Safe!"
+toilet -f bigmono9 -F gay Hello, Linux!
 
-
-
+sleep 9
+cmatrix 
