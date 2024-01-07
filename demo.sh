b9a428e21afe22465344c397dc64b90480bae9c1
Winston12334556654 <137158236+Winston12334556654@users.noreply.github.com>
2024-01-07 08:01:22 +0630

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -5,11 +5,7 @@ brew install toilet
 sudo apt-get install cmatrix
 brew install cmatrix
 
-if [ $? -ne 0 ];
-then 
-	echo "Heee Heee"
-exit 1
-fi
+
 
 #toilet "Team OKKA hacked your system!" 
 toilet -f bigmono9 -F gay Team OKKA--Hacked your system!
